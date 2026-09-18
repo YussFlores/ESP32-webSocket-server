d012b5d9f240b4aa572591eb740dfeb6f0856f6e
YussFlores <ymfes01@gmail.com>
2026-09-18 13:07:03 -0500

    Added demos

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,14 @@
 #include "webPage.h"
 #include "sensors.h"
 
+//This project was divided into two modules: hardware (Sensors.h) and the webPage (webPage.h)
+
+/* 
+  1. Initialize the webSockets server
+  2. Start reading sensors
+  3. If a connection, open the webSocket and stream the HTML, CSS, JS files
+  4. Broadcast the sensor data, and listen to messages
+*/
 const unsigned long interval = 300;
 unsigned long lastSend = 0;
 void setup(){
@@ -13,6 +21,7 @@ void setup(){
 
 void loop(){
   loopPage();
+  //Trying to not flood the page with data
   if(millis() - lastSend > interval){
     lastSend = millis();
      transmission(sensorData());
